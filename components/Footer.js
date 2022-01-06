fa37886f40efef76640abe284fba764ca5c9f4c7
Belendia Serda <belendia@gmail.com>
2022-01-06 21:16:25 +0300

    Add pages and components

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Copyright 2021 List</div>;
+};
+
+export default Footer;
---
+++ pages/test/index.js
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h1>Index in Test folder</h1>
+    </div>
+  );
+};
+
+export default index;
